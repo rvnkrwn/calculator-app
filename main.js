854022a669bf38b4adac097fce3e3c58741bd58a
Revan Kurniawan <rvnkrwn@gmail.com>
2024-01-19 01:18:09 +0700

    🐞 chore(bug): update and fix a bug

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const display = document.getElementById('display')
 
 function appendToDisplay(value) {
-    if(display.value = '0') {
+    if(display.value == '0') {
         display.value = value
     } else {
         display.value += value
