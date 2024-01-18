83297a50cc5f9a81c2ef3fbbb181bad1a24f646a
Revan Kurniawan <rvnkrwn@gmail.com>
2024-01-19 01:03:41 +0700

    🎖️ feat(function): add function calculator

---
+++ main.js
@@ -0,0 +1,28 @@
+const display = document.getElementById('display')
+
+function appendToDisplay(value) {
+    if(display.value = '0') {
+        display.value = value
+    } else {
+        display.value += value
+    }
+}
+
+function backspace() {
+    display.value = display.value.slice(0, -1)
+}
+
+function clearDisplay() {
+    display.value = '0'
+}
+
+function calculate() {
+    let expression = display.value
+    try {
+        expression = expression.replace(/%/g, '/100')
+        const result = eval(expression)
+        display.value = result
+    } catch (error) {
+        display.value = error
+    }
+}
